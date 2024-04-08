f653505a958bee6197fc6186b371642f1532b986
Kikoken <kikoken@gmail.com>
2024-04-08 10:59:58 -0400

    feat: setup eslint with typescript

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,37 @@
-import express, { Application } from 'express'
-import cors from 'cors'
-import helmet from 'helmet'
-import pino from 'pino'
+import express, { Application } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import pino from 'pino';
 
-import HealthCheck from '@interfaces/routes/healthCheck.route'
-import UserRoutes  from '@interfaces/routes/user.routes'
+import HealthCheck from '@interfaces/routes/healthCheck.route';
+import UserRoutes from '@interfaces/routes/user.routes';
 
-const app: Application = express()
-const PORT = 3000
-const logger = pino()
-const version = '/v1'
+const app: Application = express();
+const PORT = 3000;
+const logger = pino();
+const version = '/v1';
 
-app.use(express.json() )
-app.use(cors())
-app.use(helmet())
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
 
-app.use(`${version}/user`, UserRoutes)
-app.use(`${version}/health`, HealthCheck)
+app.use(`${version}/user`, UserRoutes);
+app.use(`${version}/health`, HealthCheck);
 
 // Manejo de errores global
 process.on('uncaughtException', (err) => {
-    logger.error('Uncaught Exception:', err)
-    process.exit(1);
+  logger.error('Uncaught Exception:', err);
+  process.exit(1);
 });
 
 process.on('unhandledRejection', (err) => {
-    logger.error('Unhandled Rejection:', err);
-    process.exit(1);
+  logger.error('Unhandled Rejection:', err);
+  process.exit(1);
 });
 
 app.listen(PORT, () => {
-    logger.info(`Server is running on port ${PORT}`)
+  logger.info(`Server is running on port ${PORT}`);
 }).on('error', (err) => {
-    logger.error('Error starting server:', err.message);
-    process.exit(1);
+  logger.error('Error starting server:', err.message);
+  process.exit(1);
 });
